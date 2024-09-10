49bd38d75f85a3a832e72b9ea8f3731f10a46f32
veloso <gveloso2004@gmail.com>
2024-09-09 22:42:18 -0300

    filter

---
+++ src/pages/RepositoriesPage/Filter/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Filter() {
+
+  const LPs = [
+    { name: 'JavaScript', count: '4', color: "#FCC419"},
+    { name: 'PHP', count: '5', color: '#95a5a6'}
+  ]
+
+  const qlqlr = LPs.map()
+
+  return (
+    <h1>Ola</h1>
+  )
+}
+
+export default Filter
---
+++ src/pages/RepositoriesPage/Filter/style.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: grid;
+  grid-template-columns: grid;
+  gap: 1rem;
+`;
+
+export const Selector = styled.button`
+  display: flex;
+  aling-items: center;
+  justify-content: space between;
+  width: 100%;
+`;
