16f86d56b1070f923caa9029fddd35b03870df14
veloso <gveloso2004@gmail.com>
2024-09-10 00:03:42 -0300

    langs

--- src/pages/RepositoriesPage/Filter/index.js
+++ src/pages/RepositoriesPage/Filter/index.js
@@ -2,12 +2,11 @@ import React from 'react'
 
 function Filter() {
 
-  const LPs = [
+  const langs = [
     { name: 'JavaScript', count: '4', color: "#FCC419"},
     { name: 'PHP', count: '5', color: '#95a5a6'}
   ]
 
-  const qlqlr = LPs.map()
 
   return (
     <h1>Ola</h1>
--- src/pages/RepositoriesPage/Filter/style.js
+++ src/pages/RepositoriesPage/Filter/style.js
@@ -2,13 +2,13 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   display: grid;
-  grid-template-columns: grid;
-  gap: 1rem;
+  grid-template-columns: auto;
+  gap: 0.2rem;
 `;
 
 export const Selector = styled.button`
   display: flex;
   aling-items: center;
-  justify-content: space between;
+  justify-content: space-between;
   width: 100%;
 `;
